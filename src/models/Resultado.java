f1e21e6afde85d9ad7bb5917b5e6202ffb90d124
Paola Pintado <paolapintado2409@gmail.com>
2026-06-01 08:51:29 -0500

    Metodo de comparativa

---
+++ src/models/Resultado.java
@@ -0,0 +1,53 @@
+package models;
+
+public class Resultado {
+    private String algoritmo;
+    private String escenario;
+    private int sample;
+    private double tiempoNano;
+    private  double tiempoMillis;
+    public Resultado() {
+    }
+    public Resultado(String algoritmo, String escenario, int sample, double tiempoNano) {
+        this.algoritmo = algoritmo;
+        this.escenario = escenario;
+        this.sample = sample;
+        this.tiempoNano = tiempoNano;
+        this.tiempoMillis = tiempoNano / 10000000;
+    }
+    public String getAlgoritmo() {
+        return algoritmo;
+    }
+    public void setAlgoritmo(String algoritmo) {
+        this.algoritmo = algoritmo;
+    }
+    public String getEscenario() {
+        return escenario;
+    }
+    public void setEscenario(String escenario) {
+        this.escenario = escenario;
+    }
+    public int getSample() {
+        return sample;
+    }
+    public void setSample(int sample) {
+        this.sample = sample;
+    }
+    public double getTiempoNano() {
+        return tiempoNano;
+    }
+    public void setTiempoNano(double tiempoNano) {
+        this.tiempoNano = tiempoNano;
+    }
+    public double getTiempoMillis() {
+        return tiempoMillis;
+    }
+    public void setTiempoMillis(double tiempoMillis) {
+        this.tiempoMillis = tiempoMillis;
+    }
+
+    
+
+    
+
+}
